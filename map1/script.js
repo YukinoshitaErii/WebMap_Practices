93c1f0fe2ea3fa47143edd66e88345afc2aff65d
Christ-F <ChristF008@outlook.com>
2023-02-22 11:19:23 +0000

    lab4 2nd

--- map1/script.js
+++ map1/script.js
@@ -1,19 +1,38 @@
 // The value for 'accessToken' begins with 'pk...'
-// mapboxgl.accessToken =
-//     "pk.eyJ1IjoiY2hyaXN0MDAwIiwiYSI6ImNsY3E0Y2IzbzAyYzkzc296bnR1bjN1em8ifQ.ifAwJWZFi6RRQCXNH3AYfg";
+mapboxgl.accessToken =
+    "pk.eyJ1IjoiY2hyaXN0MDAwIiwiYSI6ImNsY3E0Y2IzbzAyYzkzc296bnR1bjN1em8ifQ.ifAwJWZFi6RRQCXNH3AYfg";
+
+// create map
+const map = new mapboxgl.Map({
+    container: "map", // container element id
+    style: "mapbox://styles/christ000/cldrevbcq000901qr5wlrjeed",
+    center: [-73.890,40.834], // initial map center in [lon, lat]
+    zoom: 12
+});
+
+// // config map
+// // 10 is free
+// let config = {
+//     minZoom: 2,
+//     maxZoom:10
+// };
 //
-// // create map
-// const map = new mapboxgl.Map({
-//     container: "map", // container element id
-//     style: "mapbox://styles/christ000/cldrevbcq000901qr5wlrjeed",
-//     center: [-73.890,40.834], // initial map center in [lon, lat]
-//     zoom: 12
-// });
+// // magnification with which the map will start
+// const zoom = 4;
+// // co-ordinates
+// const lat = 40.834;
+// const long = -73.890;
 
+const data_url =
+    "https://raw.githubusercontent.com/YukinoshitaErii/WebMap_Practices/main/list.json";
 
-// run python script
-let jsexecpy = require("jsexecpy")
-let callback = function({data,pythonpath},otherargs = 11){
-dosomething(data,pythonpath,otherargs)
-};
-jsexecpy.runpath("/Users/000fere/Documents/Python_Projects/I");
+let geoJson;
+
+// fetch data list
+fetch(data_url)
+    .then(function (response){
+        return response.json();
+    }).then(function (json){
+        console.log(json);
+});
+
